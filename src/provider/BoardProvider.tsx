b8bf216ee8842d0a78f4dd3d22981975599a0e7c
Akshat Sharma <webdevakshat@gmail.com>
2024-07-23 17:04:31 +0530

    Fixed provider bug

--- src/provider/BoardProvider.tsx
+++ src/provider/BoardProvider.tsx
@@ -1,4 +1,10 @@
-import {type FC, type ReactNode, useEffect, useReducer, useState } from "react";
+import {
+  type FC,
+  type ReactNode,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
 import { BoardContext } from "../context/BoardContext";
 import localforage from "localforage";
 import type { Board } from "../lib/types";
@@ -6,45 +12,44 @@ import { reducer, initialState } from "../reducers/boardReducer";
 import defaultBoards from "../lib/data";
 
 interface BoardProviderProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 export const BoardProvider: FC<BoardProviderProps> = ({ children }) => {
-    const [state, dispatch] = useReducer(reducer, initialState);
-    const [isInitialized, setIsInitialized] = useState(false);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [isInitialized, setIsInitialized] = useState(false);
 
-    useEffect(() => {
-        const initializeBoards = async () => {
-            const storedBoards: Board[] | null = await localforage.getItem("boards");
-            if (!storedBoards) {
-                dispatch({
-                    type: "SET_INITIAL_STATE",
-                    payload: {boards : defaultBoards},
-                });
-            } else {
-                dispatch({
-                    type: "SET_INITIAL_STATE",
-                    payload: {boards : storedBoards},
-                });
-            }
-            setIsInitialized(true);
-        };
+  useEffect(() => {
+    const initializeBoards = async () => {
+      const storedBoards: Board[] | null = await localforage.getItem("boards");
+      if (!storedBoards) {
+        dispatch({
+          type: "SET_INITIAL_STATE",
+          payload: { boards: defaultBoards },
+        });
+      } else {
+        dispatch({
+          type: "SET_INITIAL_STATE",
+          payload: { boards: storedBoards },
+        });
+      }
+      setIsInitialized(true);
+    };
 
-        initializeBoards();
-    }, [dispatch]);
+    initializeBoards();
+  }, [dispatch]);
 
-    useEffect(() => {
-        localforage.setItem("boards", state.boards);
-    } , [state])
+  useEffect(() => {
+    state.boards.length > 0 && localforage.setItem("boards", state.boards);
+  }, [state]);
 
-    if (!isInitialized) {
-        return null;
-    }
+  if (!isInitialized) {
+    return null;
+  }
 
-    return (
-        <BoardContext.Provider value={{ state, dispatch }}>
-            {children}
-        </BoardContext.Provider>
-    );
+  return (
+    <BoardContext.Provider value={{ state, dispatch }}>
+      {children}
+    </BoardContext.Provider>
+  );
 };
-
